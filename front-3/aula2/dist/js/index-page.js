c3500848b8ebbfcd9dccab64ab3394eb13a707bc
Roger Medeiros <roger@roger.dev.br>
2022-09-22 22:29:49 -0300

    refatoraçao do codigo

--- front-3/aula2/dist/js/index-page.js
+++ front-3/aula2/dist/js/index-page.js
@@ -1,7 +1,57 @@
+import { lerAlunos } from "./repositorio.js";
 console.log("Typescript ok");
-window.addEventListener("load", (ev) => {
-    console.log("carregado");
+window.addEventListener("DOMContentLoaded", () => {
+    // com certeza todos elementos vão estar disponiveis
+    // 1 - Buscar os dados no repositorio
+    // 2 - Atualizar a tabela
+    const alunos = lerAlunos();
+    montarTabela(alunos);
 });
+function criarColuna(texto) {
+    const coluna = document.createElement("td");
+    // coluna.classList.add("bg-info");
+    coluna.innerText = texto;
+    return coluna;
+}
+function adicionaClasses(elemento, classes) {
+    for (const classe of classes) {
+        elemento.classList.add(classe);
+    }
+}
+function montarTabela(alunos) {
+    var _a;
+    const tableBody = document.getElementById("table-body");
+    const tableBodyNew = document.createElement("tbody");
+    alunos.forEach((aluno) => {
+        const tr = document.createElement("tr");
+        const nomeTd = criarColuna(aluno.nome);
+        const cursoTd = criarColuna(aluno.curso);
+        const acoesTd = criarColuna("");
+        adicionaClasses(acoesTd, ["bg-primary"]);
+        const editBtn = document.createElement("button");
+        //editBtn.classList.add("btn");
+        //editBtn.classList.add("btn-primary");
+        adicionaClasses(editBtn, ["btn", "btn-warning"]);
+        editBtn.innerText = "Editar";
+        editBtn.setAttribute("onclick", "editarAluno");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("btn");
+        deleteBtn.classList.add("btn-danger");
+        deleteBtn.innerText = "Apagar";
+        deleteBtn.setAttribute("onclick", "deletarAluno");
+        tr.appendChild(nomeTd);
+        tr.appendChild(cursoTd);
+        acoesTd.appendChild(editBtn);
+        acoesTd.appendChild(deleteBtn);
+        tr.appendChild(acoesTd);
+        tableBodyNew.appendChild(tr);
+    });
+    if (tableBody) {
+        (_a = tableBody.parentNode) === null || _a === void 0 ? void 0 : _a.replaceChild(tableBodyNew, tableBody);
+    }
+}
+function editarAluno() { }
+function deletarAluno() { }
 const inputSearch = document.getElementById("input-search");
 inputSearch.addEventListener("keyup", (ev) => {
     // sempre que o usuário apertar e soltar um tecla
@@ -12,10 +62,8 @@ inputSearch.addEventListener("keyup", (ev) => {
             inputSearch.value = "";
             break;
         case "Enter":
-            // fakeData();
             break;
         default:
             break;
     }
 });
-export {};
--- front-3/aula2/src/index-page.ts
+++ front-3/aula2/src/index-page.ts
@@ -1,12 +1,80 @@
-import { fakeData, lerAlunos } from "./repositorio.js";
+import { Aluno } from "./aluno.interface.js";
+import { lerAlunos, fakeData } from "./repositorio.js";
 
 console.log("Typescript ok");
 
-window.addEventListener("DOMContentLoaded", (ev: Event) => {
+window.addEventListener("DOMContentLoaded", () => {
   // com certeza todos elementos vão estar disponiveis
   // 1 - Buscar os dados no repositorio
   // 2 - Atualizar a tabela
+
+  const alunos = lerAlunos();
+
+  montarTabela(alunos);
 });
+
+function criarColuna(texto: string): HTMLTableCellElement {
+  const coluna = document.createElement("td") as HTMLTableCellElement;
+  // coluna.classList.add("bg-info");
+  coluna.innerText = texto;
+
+  return coluna;
+}
+
+function adicionaClasses(elemento: HTMLElement, classes: string[]) {
+  for (const classe of classes) {
+    elemento.classList.add(classe);
+  }
+}
+
+function visibilidadeElemento(elemento: HTMLElement, visivel: boolean) {
+  //
+}
+
+function montarTabela(alunos: Aluno[]): void {
+  const tableBody = document.getElementById("table-body") as HTMLTableSectionElement;
+  const tableBodyNew = document.createElement("tbody") as HTMLTableSectionElement;
+
+  alunos.forEach((aluno) => {
+    const tr = document.createElement("tr") as HTMLTableRowElement;
+
+    const nomeTd = criarColuna(aluno.nome);
+    const cursoTd = criarColuna(aluno.curso);
+    const acoesTd = criarColuna("");
+    adicionaClasses(acoesTd, ["bg-primary"]);
+
+    const editBtn = document.createElement("button") as HTMLButtonElement;
+    //editBtn.classList.add("btn");
+    //editBtn.classList.add("btn-primary");
+    adicionaClasses(editBtn, ["btn", "btn-warning"]);
+    editBtn.innerText = "Editar";
+    editBtn.setAttribute("onclick", "editarAluno");
+
+    const deleteBtn = document.createElement("button") as HTMLButtonElement;
+    deleteBtn.classList.add("btn");
+    deleteBtn.classList.add("btn-danger");
+    deleteBtn.innerText = "Apagar";
+    deleteBtn.setAttribute("onclick", "deletarAluno");
+
+    tr.appendChild(nomeTd);
+    tr.appendChild(cursoTd);
+
+    acoesTd.appendChild(editBtn);
+    acoesTd.appendChild(deleteBtn);
+
+    tr.appendChild(acoesTd);
+
+    tableBodyNew.appendChild(tr);
+  });
+
+  if (tableBody) {
+    tableBody.parentNode?.replaceChild(tableBodyNew, tableBody);
+  }
+}
+
+function editarAluno() {}
+
+function deletarAluno() {}
 
 const inputSearch = document.getElementById("input-search") as HTMLInputElement;
 inputSearch.addEventListener("keyup", (ev: KeyboardEvent) => {
@@ -18,7 +86,6 @@ inputSearch.addEventListener("keyup", (ev: KeyboardEvent) => {
       inputSearch.value = "";
       break;
     case "Enter":
-      // fakeData();
       break;
 
     default:
