00b4c96d1d1d247651543e8e41f53cd4ba329837
Roger Medeiros <roger@roger.dev.br>
2022-11-08 21:53:25 -0300

    post

--- front-3/aula10/src/index.ts
+++ front-3/aula10/src/index.ts
@@ -13,3 +13,35 @@ app.listen(3300, () => {
 app.get("/", (req: Request, res: Response) => {
   res.send("<h3>🤘 API Funcionando</h3>");
 });
+
+interface Produto {
+  id: number;
+  descricao: string;
+  preco: number;
+  disponivel: boolean;
+}
+
+const lista: Produto[] = [];
+
+// Habilitar o backend para receber JSON na requisiçao
+app.use(express.json());
+
+// Criar uma rota para inserir um novo produto
+app.post("/produtos", (req: Request, res: Response) => {
+  // Desestruturo a requisição para pegar o corpo
+  const { descricao, preco, disponivel } = req.body;
+
+  // Crio o novo produto
+  const novo = {
+    id: lista.length + 1,
+    descricao,
+    preco,
+    disponivel,
+  };
+
+  // adiciono na lista
+  lista.push(novo);
+
+  // respondo em formato json
+  res.json(novo);
+});
