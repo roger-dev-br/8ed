145f0a580497357529f6a948a8b890934902c738
Roger Medeiros <roger@roger.dev.br>
2022-12-01 16:34:32 -0300

    aula 4

---
+++ backend/aula4/src/data/products-list.ts
@@ -0,0 +1,74 @@
+const productsList = [
+  {
+    id: 1,
+    description: "Limão",
+    price: 6.59,
+    stock: 45,
+    sale: true,
+  },
+  {
+    id: 2,
+    description: "Banana",
+    price: 4.7,
+    stock: 450,
+    sale: true,
+  },
+  {
+    id: 3,
+    description: "Maça",
+    price: 9.15,
+    stock: 50,
+    sale: true,
+  },
+  {
+    id: 4,
+    description: "Farinha",
+    price: 3.73,
+    stock: 1200,
+    sale: false,
+  },
+  {
+    id: 5,
+    description: "Açucar",
+    price: 4.8,
+    stock: 450,
+    sale: true,
+  },
+  {
+    id: 6,
+    description: "Mamão",
+    price: 11,
+    stock: 10,
+    sale: true,
+  },
+  {
+    id: 7,
+    description: "Cerveja",
+    price: 5.85,
+    stock: 100,
+    sale: true,
+  },
+  {
+    id: 8,
+    description: "Vodka",
+    price: 23.3,
+    stock: 0,
+    sale: false,
+  },
+  {
+    id: 9,
+    description: "Chocolate",
+    price: 6.49,
+    stock: 32,
+    sale: true,
+  },
+  {
+    id: 10,
+    description: "Leite",
+    price: 4.11,
+    stock: 1200,
+    sale: true,
+  },
+];
+
+export default productsList;
