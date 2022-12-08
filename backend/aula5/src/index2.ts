2bd542b230e94c4267ebc2c8369fc259137289e6
Roger Medeiros <roger@roger.dev.br>
2022-12-08 19:30:21 -0300

    fix: aula 5

---
+++ backend/aula5/src/index2.ts
@@ -0,0 +1,61 @@
+import express, { Express, Request, Response } from "express";
+
+// criar um instancia do express
+const server: Express = express();
+
+// escuto uma porta e o express responde nela
+server.listen(3000, () => {
+  console.log("Servidor OK");
+});
+
+server.get("/", (req: Request, res: Response) => {
+  // processa a requisiçao e devolve uma resposta
+  res.send("<h1>Api Funcionando</h1>");
+});
+
+server.get("/users", (req: Request, res: Response) => {
+  res.json([
+    {
+      id: 1,
+      name: "Rogerio",
+    },
+    {
+      id: 2,
+      name: "Luan",
+    },
+    {
+      id: 3,
+      name: "Fabiane",
+    },
+  ]);
+});
+
+server.get("/posts", (req: Request, res: Response) => {
+  // obter as informaçoes de query params
+  const { query } = req;
+
+  if (!query.autor) {
+    return res.status(400).json({
+      success: false,
+      message: "Autor required",
+    });
+  }
+
+  // processa a requisiçao e devolve uma resposta
+  res.send(`Recebi o Autor ${query.autor} ano ${query.ano ?? "nao informou"}`);
+});
+
+server.get("/posts/:codigo", (req: Request, res: Response) => {
+  // obter as informaçoes de query params
+  const { params } = req;
+
+  if (params.codigo === "10") {
+    return res.status(404).json({
+      success: false,
+      message: "Post not found",
+    });
+  }
+
+  // processa a requisiçao e devolve uma resposta
+  res.send(`Requisitaram o post ${params.codigo}`);
+});
