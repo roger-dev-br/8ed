87ed700dd41b3408e73e09e8a0a16bdedfdc4039
Roger Medeiros <roger@roger.dev.br>
2022-12-15 20:13:20 -0300

    middlewares

---
+++ backend/aula5/middl.md
@@ -0,0 +1,5 @@
+
+Middleware 1 - use
+Middleware 2 - use
+Requisição alterada - impresso pelo middlewar 2
+Middleware Get PETS - rota específica GET /pets/:codigo
--- backend/aula5/src/index.ts
+++ backend/aula5/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 
 // criar um instancia do express
@@ -33,6 +33,53 @@ interface ResponstaPadrao {
 
 const pets: Pet[] = [];
 
+function imprimirDataHora(req: any, res: Response, next: NextFunction) {
+  req.dataHora = new Date();
+  req.dados = "Requisição alterada";
+  console.log("Middleware 1");
+
+  next();
+}
+
+function middleware2(req, res, next) {
+  console.log("Middleware 2");
+  console.log(req.dados);
+  next();
+}
+
+function verficaSePetExiste(req, res, next) {
+  const { codigo } = req.params;
+
+  // procuro se o pet está no Array
+  const pet = pets.find((f) => f.codigo === Number(codigo));
+  if (!pet) {
+    // Aborto a requisição e envio pro cliente a informação
+    return res.status(404).json({
+      sucesso: false,
+      mensagem: "Pet não encontrado",
+    } as ResponstaPadrao);
+  }
+
+  next();
+}
+
+function middlewareGetPets(req, res, next) {
+  console.log("Middleware Get PETS");
+
+  next();
+}
+
+function logAcesso(req: Request, res: Response, next: NextFunction) {
+  console.log("Middlewar log Acesso");
+  console.log(req.originalUrl);
+  console.log(new Date());
+
+  next();
+}
+
+//server.use(imprimirDataHora, middleware2);
+//server.use(middleware2);
+
 // POST - Adiciona um novo PET a Lista
 server.post("/pets", (req: Request, res: Response) => {
   const { nome, observacao } = req.body;
@@ -63,12 +110,10 @@ server.post("/pets", (req: Request, res: Response) => {
     data: pet,
   } as ResponstaPadrao);
 });
+server.use(logAcesso);
 
 server.get("/pets", (req: Request, res: Response) => {
   const { filtro } = req.query;
-
-  console.log(filtro);
-
   if (filtro) {
     return res.json({
       sucesso: true,
@@ -82,16 +127,10 @@ server.get("/pets", (req: Request, res: Response) => {
   } as ResponstaPadrao);
 });
 
-server.get("/pets/:codigo", (req: Request, res: Response) => {
+server.get("/pets/:codigo", [middlewareGetPets, verficaSePetExiste], (req: Request, res: Response) => {
   const { codigo } = req.params;
 
   const pet = pets.find((f) => f.codigo === Number(codigo));
-  if (!pet) {
-    return res.status(404).json({
-      sucesso: false,
-      mensagem: "Pet não encontrado",
-    } as ResponstaPadrao);
-  }
 
   res.status(200).json({
     sucesso: true,
@@ -118,19 +157,13 @@ server.delete("/pets/:codigo", (req: Request, res: Response) => {
   } as ResponstaPadrao);
 });
 
-server.put("/pets/:codigo", (req: Request, res: Response) => {
+server.put("/pets/:codigo", verficaSePetExiste, (req: Request, res: Response) => {
   const { codigo } = req.params;
   const { observacao } = req.body;
 
   const pet = pets.find((f) => f.codigo === Number(codigo));
-  if (!pet) {
-    return res.status(404).json({
-      sucesso: false,
-      mensagem: "Pet não encontrado",
-    } as ResponstaPadrao);
-  }
 
-  pet.observacao = observacao;
+  pet!.observacao = observacao;
   res.status(200).json({
     sucesso: true,
     data: pet,
