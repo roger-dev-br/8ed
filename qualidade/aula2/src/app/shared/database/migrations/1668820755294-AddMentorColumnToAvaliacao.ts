cb68d1557b866415111404ecc0cd2f53ab1f5a85
Roger Medeiros <roger@roger.dev.br>
2023-03-23 19:44:18 -0300

    migrations ok

--- qualidade/aula2/src/app/shared/database/migrations/1668820755294-AddMentorColumnToAvaliacao.ts
+++ qualidade/aula2/src/app/shared/database/migrations/1668820755294-AddMentorColumnToAvaliacao.ts
@@ -1,14 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddMentorColumnToAvaliacao1668820755294 implements MigrationInterface {
-    name = 'AddMentorColumnToAvaliacao1668820755294'
+  name = "AddMentorColumnToAvaliacao1668820755294";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "aula"."avaliacao" ADD "mentor" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "avaliacao" ADD "mentor" character varying`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "aula"."avaliacao" DROP COLUMN "mentor"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "avaliacao" DROP COLUMN "mentor"`);
+  }
 }
--- qualidade/aula2/src/main/config/database.config.ts
+++ qualidade/aula2/src/main/config/database.config.ts
@@ -1,26 +1,28 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
+import { FirstMigration1668820170473 } from "../../app/shared/database/migrations/1668820170473-FirstMigration";
+import { AddMentorColumnToAvaliacao1668820755294 } from "../../app/shared/database/migrations/1668820755294-AddMentorColumnToAvaliacao";
 
 let dataSource = new DataSource({
-    type: "postgres",
-    url: process.env.DB_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-    synchronize: false,
-    entities: ["src/app/shared/database/entities/**/*.ts"],
-    migrations: ["src/app/shared/database/migrations/**/*.ts"],
-    schema: "aula",
+  type: "postgres",
+  url: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: false,
+  entities: ["src/app/shared/database/entities/**/*.ts"],
+  migrations: [FirstMigration1668820170473, AddMentorColumnToAvaliacao1668820755294],
+  schema: "public",
 });
 
 if (process.env.NODE_ENV === "test") {
-    dataSource = new DataSource({
-        type: "sqlite",
-        database: "database.sqlite3",
-        synchronize: false,
-        entities: ["src/app/shared/database/entities/**/*.ts"],
-        migrations: ["tests/shared/migrations/**/*.ts"],
-    });
+  dataSource = new DataSource({
+    type: "sqlite",
+    database: "database.sqlite3",
+    synchronize: false,
+    entities: ["src/app/shared/database/entities/**/*.ts"],
+    migrations: [FirstMigration1668820170473, AddMentorColumnToAvaliacao1668820755294],
+  });
 }
 
 export default dataSource;
